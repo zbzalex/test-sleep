40423e4de7d897cf89fb747a0a1a896187981640
macbook15 <macbook15@MacBook-Pro-macbook15.local>
2023-06-05 20:12:26 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function sleep(millis) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve(), millis)
+  });
+}
+
+let t = Date.now()
+sleep(100).then(() => console.log(Date.now() - t))
